c56621ab3e24df5ce59b37775595662cb282bb7c
Agus Ibrahim <mynameisagoes@facebook.com>
2015-03-06 14:14:27 +0700

    Add barcodescaner, popup profile, small fix

--- assets/www/cordova_plugins.js
+++ assets/www/cordova_plugins.js
@@ -1,17 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
-        "id": "com.ionic.keyboard.keyboard",
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
         "clobbers": [
-            "cordova.plugins.Keyboard"
+            "cordova.plugins.barcodeScanner"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.ionic.keyboard": "0.0.1"
+    "com.phonegap.plugins.barcodescanner": "2.0.1"
 }
 // BOTTOM OF METADATA
 });
--- assets/www/js/my-app.js
+++ assets/www/js/my-app.js
@@ -6,7 +6,13 @@ document.addEventListener("deviceready", function (){
 	//myApp.alert("Ready");
 });
 
+// Tangani tombol back
+document.addEventListener("backbutton", function (){
+	//temtukan disini
+});
+
 // Tangani saat keyboard show/hide
+// Menggunakan plugin keyboard dari ionic
 // Untuk memperbaiki masalah fokus pada input saat keyboard muncul
 // Sibet sih, tapi ampuh
 window.addEventListener('native.keyboardshow', keyboardShowHandler);
@@ -20,6 +26,7 @@ function keyboardHideHandler(e){
 	  $$(".page-content").attr("style", "");
   }
 }
+
 // Definisikan app
 var myApp = new Framework7({
 	swipePanel: "left"
@@ -31,34 +38,40 @@ var $$ = Dom7;
 $$(document).on('ajaxStart', function (e) {
     myApp.showPreloader("Mengambil data...");
 });
+
 // Jika ajax selesai di hide preloadernya
 $$(document).on('ajaxComplete', function () {
     myApp.hidePreloader();
 });
+
 // Main view
 var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true,
 });
+
 // Batasi jumalah karakter input NISN, serta sembunyikan tombol titik
 myApp.keypad({
 	input: '#nisn',
 	valueMaxLength: 10,
 	dotButton: false,
 });
+
 // Saat Accordion "NISN" dibuka, tutup accordion "Nama" (jika sedang terbuka)
 $$("#bynisn").on("open", function (){
 	if ($$("#byname").hasClass("accordion-item-expanded")){
 		myApp.accordionClose("#byname");
 	}
 });
+
 // Saat Accordion "Nama" dibuka, tutup accordion "NISN" (jika sedang terbuka)
 $$("#byname").on("open", function (){
 	if ($$("#bynisn").hasClass("accordion-item-expanded")){
 		myApp.accordionClose("#bynisn");
 	}
 });
-// tangani accordion opened dan close
-// untuk keperluan hide/show tombol kirim
+
+// Tangani accordion opened dan close
+// Untuk keperluan hide/show tombol login
 $$("#byname").on("close", function (){
 	$$("#nama").change();
 });
@@ -73,11 +86,12 @@ $$("#byname").on("opened", function (){
 });
 
 // Input kalender
-var calendarDefault = myApp.calendar({
+myApp.calendar({
     input: '#tanggalan'
 });
 
-// Tombol kirim di klik
+// Tombol login di klik
+// Masih belum bekerja ;)
 $$('#kirim').on('click', function () {
   if (!$$(".accordion-item-expanded").html()){
 	  myApp.alert("Pilih metode masuk");
@@ -91,7 +105,7 @@ $$('#kirim').on('click', function () {
 });
 
 // Saat mengisi/mengetik NISN
-// jika panjang NISN=10 maka tombol kirim akan enable dan sebaliknya
+// jika panjang NISN=10 maka tombol login akan enable dan sebaliknya
 $$("#nisnmode").on("change", function (){
 	if ($$("#bynisn").hasClass("accordion-item-expanded")){
 		if ($$("#nisn").val().length==10){
@@ -103,8 +117,9 @@ $$("#nisnmode").on("change", function (){
 		$$("#kirim").attr("disabled","");
 	}
 });
-// Saat mengisi/mengetik Nama/TTL/Tanggal lahir
-// jika panjang Nama>3, panjang TTL>3 dan panjang Tanggal>8 maka tombol kirim akan enable dan sebaliknya
+
+// Saat mengisi/mengetik Nama/TL/Tanggal lahir
+// jika panjang Nama>3, panjang TL>3 dan panjang Tanggal>8 maka tombol login akan enable dan sebaliknya
 $$("#namemode").on("change", function (){
 	if ($$("#byname").hasClass("accordion-item-expanded")){
 		if ($$("#nama").val().length>3 && $$("#tlahir").val().length>3 && $$("#tanggalan").val().length>8){
@@ -116,3 +131,35 @@ $$("#namemode").on("change", function (){
 		$$("#kirim").attr("disabled","");
 	}
 });
+
+// Fungsi Scan Barcode
+// Menggunakan plugin barcodescanner (Zxing)
+$$("#scanbar").on("click", function (){
+	cordova.plugins.barcodeScanner.scan(
+      function (result) {
+		  // jika user tidak mencancel
+		  if (!result.cancelled){
+			  // set result ke #nisn
+			  $$("#nisn").val(result.text);
+		  }
+      },
+	  // scan error
+      function (error) {
+          myApp.alert("Scanning failed: " + error);
+      }
+   );
+});
+function showProfile(data){
+	var prof='<div class="popup popup-profile">'+
+	  '<div id="wrapper">'+
+		'<div id="profile">'+
+		  '<div class="img"></div>'+
+		  '<div class="info">'+
+			'<strong>Jack Sparrow</strong>. I\'m a <strong>50 year old</strong> self-employed <strong>Pirate</strong> from the <strong>Caribbean</strong>.'+
+		  '</div>'+
+		'</div>'+
+		'<a href="#" class="button button-fill color-red close-popup">Close</a>'+
+	  '</div>'+
+    '</div>';
+	myApp.popup(prof);
+}
